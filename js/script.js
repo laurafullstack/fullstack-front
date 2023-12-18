f0842d94bc8bbac770de4b03fae26726ca5b89ea
laurafullstack <laurapajaresmartin@hotmail.com>
2023-12-18 14:22:19 +0100

    modificaciones html y js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ document.getElementById("formulario").addEventListener("submit",
         }  
     }
 );
+
+$("#medida").on('change', function() {
+    console.log(this.value);
+    // Aqui va la llamada AJAX con JQuery
+    $.ajax({
+        url:'http://localhost:5000/checksize', 
+        type: 'POST',
+        dataType: 'html',
+        success: function(){
+            if(document.getElementById("medida").value == "S"){
+                document.getElementById("resultado_medida").innerHTML = "No disponible";
+                event.preventDefault();
+            }
+        }
+    });
+});
+   
